f77f7bbd05c757c1f76b756d4a2cbd17c4db8999
Team Neiman <136953008+NEIMAN-AI@users.noreply.github.com>
2023-09-07 13:39:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,66 +1,24 @@
 import os
+from os import getenv
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
 
 
-class Config(object):
-    # Editable Variables #
-    ABUSE = os.environ.get("ABUSE", None)
-    API_HASH = os.environ.get("API_HASH", None)
-    APP_ID = os.environ.get("APP_ID", None)
-    BL_CHAT = set(int(x) for x in os.environ.get("BL_CHAT", "").split())
-    BOT_HANDLER = os.environ.get("BOT_HANDLER", "\/")
-    BOT_LIBRARY = os.environ.get("BOT_LIBRARY", None)
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
-    BUTTONS_IN_HELP = int(os.environ.get("BUTTONS_IN_HELP", 7))
-    CURRENCY_API = os.environ.get("CURRENCY_API", None)
-    DATABASE_URL = os.environ.get("DATABASE_URL", None)
-    EMOJI_IN_HELP = os.environ.get("EMOJI_IN_HELP", "✧")
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-    GIT_REPO_NAME = os.environ.get("GIT_REPO", None)
-    GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
-    HANDLER = os.environ.get("HANDLER", ".")
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    IG_SESSION = os.environ.get("INSTAGRAM_SESSION", None)
-    INSTANT_BLOCK = os.environ.get("INSTANT_BLOCK", None)
-    LOGGER_ID = int(os.environ.get("LOGGER_ID", 0))
-    LYRICS_API = os.environ.get("LYRICS_API", None)
-    MAX_SPAM = int(os.environ.get("MAX_SPAM", 3))
-    MY_CHANNEL = os.environ.get("YOUR_CHANNEL", "TeamNeiman")
-    MY_GROUP = os.environ.get("YOUR_GROUP", "Neiman_X_Support")
-    OCR_API = os.environ.get("OCR_API", None)
-    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", 0))
-    PM_LOGGER = int(os.environ.get("PM_LOGGER", 0))
-    PM_PERMIT = os.environ.get("PM_PERMIT", "True")
-    REMOVE_BG_API = os.environ.get("REMOVE_BG_API", None)
-    SESSION = os.environ.get("SESSION", None)
-    SHORTENER_API = os.environ.get("SHORTENER_API", None)
-    SUDO_HANDLER = os.environ.get("SUDO_HANDLER", ".")
-    TAG_LOGGER = int(os.environ.get("TAG_LOGGER", 0))
-    THUMB_IMG = os.environ.get("THUMB_IMG", "./HellConfig/resources/pics/hellbot_logo.jpg")
-    UNLOAD = list(os.environ.get("UNLOAD", "").split())
-    UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/NEIMAN-AI/TeamNeiman")
-    UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "master")
-    WEATHER_API = os.environ.get("WEATHER_API", None)
+API_ID = int(getenv("API_ID", "6435225")) #optional
+API_HASH = getenv("API_HASH", "") #optional
 
-    # Don't edit variables below this line #
-    LOGGER = True
-    CHROME_BIN = os.environ.get("CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
-    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", "/app/.chromedriver/bin/chromedriver")
-    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
-    MAX_MESSAGE_SIZE_LIMIT = 4095
-    SUDO_USERS = []
-    TELEGRAPH_NAME = os.environ.get("TELEGRAPH_NAME", "[ †he Hêllẞø† ]")
-    TEMP_DIR = os.environ.get("TEMP_DIR", None)
-    TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
-    
-
-
-class Production(Config):
-    LOGGER = False
-
-
-class Development(Config):
-    LOGGER = True 
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+OWNER_ID = int(getenv("OWNER_ID"))
+MONGO_URL = getenv("MONGO_URL")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+ALIVE_PIC = getenv("ALIVE_PIC", '')
+ALIVE_TEXT = getenv("ALIVE_TEXT")
+PM_LOGGER = getenv("PM_LOGGER")
+LOG_GROUP = getenv("LOG_GROUP")
+GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
+REPO_URL = getenv("REPO_URL", "https://github.com/RADHAK8/RADHA-USERBOT")
+BRANCH = getenv("BRANCH", "master") #don't change
+ 
+SESSION = getenv("SESSION", "")
