cef0c7f1579e5ab4e0bc69a8379497bd5410d4b3
Team Neiman <136953008+NEIMAN-AI@users.noreply.github.com>
2023-09-07 14:57:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "27995073")) #optional
+API_HASH = getenv("API_HASH", "f57594c795197deecde36a56cc9623a6") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
